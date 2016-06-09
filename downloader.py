940a83242a6a724f6f745af57f85165a54eb90ec
IanLondon <ianlondondu@gmail.com>
2016-06-09 14:18:17 -0400

    generate patches

--- downloader.py
+++ downloader.py
@@ -1,18 +1,19 @@
 import flickrapi
 
 import secrets
-from img_tools import save_urls_to_file, download_all_imgs
+from img_tools import save_urls_to_file, download_all_imgs, save_patches
 
 # Declare flickr api object
 flickr = flickrapi.FlickrAPI(secrets.KEY, secrets.SECRET, format='parsed-json')
 
 if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser(description='Save urls to file and/or scrape urls in that file')
+    parser = argparse.ArgumentParser(description='Save urls to file and/or scrape urls in that file, using parameters in config.py')
     parser.add_argument('--url_file', default='flickr_face_urls.txt', help='Text file to store image urls')
     parser.add_argument('--tags', nargs='+', default=['face','portrait','people'], help='Tags used to search for images')
     parser.add_argument('--save_urls', action='store_true', help='If included, save urls to url_file')
     parser.add_argument('--download', action='store_true', help='If included, download images using urls in url_file')
+    parser.add_argument('--gen_patches', action='store_true', help='If included, generate patches for each image')
 
     args = parser.parse_args()
 
@@ -24,3 +25,5 @@ if __name__ == '__main__':
             save_urls_to_file(flickr, args.url_file, args.tags)
         if args.download:
             download_all_imgs(args.url_file)
+        if args.gen_patches:
+            save_patches
